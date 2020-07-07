b31dc57d48033cadc492c699d91463aeacfb58e5
Nakul Tyagi <31548192+NakulTyagi@users.noreply.github.com>
2020-07-07 18:21:57 +0530

    layout

--- made-easy/src/app/app-routing.module.ts
+++ made-easy/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
+import { MapsComponent } from './components/maps/maps.component';
+import { TranslateComponent } from './components/translate/translate.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+  pathMatch: 'full'
+},
+{
+  path: 'home',
+  component: HomeComponent,
+
+},
+{
+  path: 'bootstrap',
+  component: BootstrapComponent,
+
+},
+{
+  path: 'translate',
+  component: TranslateComponent,
+
+}, {
+  path: 'maps',
+  component: MapsComponent,
+
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- made-easy/src/app/app.module.ts
+++ made-easy/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
+import { MapsComponent } from './components/maps/maps.component';
+import { TranslateComponent } from './components/translate/translate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    BootstrapComponent,
+    MapsComponent,
+    TranslateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
